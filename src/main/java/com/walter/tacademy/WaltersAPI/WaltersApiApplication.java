2d4c4c5cfc54d4578408eef380ba0be49b2c447b
walter.hwang <lyvius2@naver.com>
2019-04-22 00:57:56 +0900

    initial commit

---
+++ src/main/java/com/walter/tacademy/WaltersAPI/WaltersApiApplication.java
@@ -0,0 +1,13 @@
+package com.walter.tacademy.WaltersAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaltersApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaltersApiApplication.class, args);
+	}
+
+}
